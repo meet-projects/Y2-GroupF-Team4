61bcfbb83d5b701f30448f7beb42b26cbf599600
boulosQ24-meet <boulosbqurbashi@gmail.com>
2023-07-30 15:26:01 +0300

    template

--- app.py
+++ app.py
@@ -10,8 +10,25 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 def home():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
+@app.route("/courses", methods=['GET','POST'])
+def courses():
+    return render_template('courses.html')
 
+@app.route('/trainers', methods=['GET', 'POST'])
+def trainers():
+    return render_template('trainers.html')
+
+@app.route("/events", methods=['GET','POST'])
+def events():
+    return render_template('events.html')
+
+@app.route("/pricing", methods=['GET','POST'])
+def pricing():
+    return render_template('pricing.html')
 #Code goes above here
 
 if __name__ == '__main__':
